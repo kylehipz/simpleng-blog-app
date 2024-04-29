a0429326537f4978c97d9d8336ef6d52b55d451f
Kyle Hipolito <kylehipolito2109@gmail.com>
2024-04-29 20:46:34 +0800

    fix: Follow logic

--- home-feed/cmd/main.go
+++ home-feed/cmd/main.go
@@ -6,18 +6,24 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v3"
+	"simpleng-blog-app/common/pkg/database"
+
+	"simpleng-blog-app/home-feed/api"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 	app := fiber.New()
 
+	err := database.Connect()
+	if err != nil {
+		log.Fatal("Error connecting to the database")
+	}
+
+	log.Println("Database connected")
+
 	v1 := app.Group("/api/v1/home-feed")
-	v1.Get("/", func(c fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"data": "home-feed",
-		})
-	})
+	v1.Get("/", api.GetHomeFeedHandler)
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%v", port)))
 }
